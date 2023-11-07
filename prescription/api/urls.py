0be48b230cd52ae4534962a68720fefe67012a84
solomon141 <weymit2001@gmail.com>
2023-11-07 16:35:36 +0300

    api_view decorators

--- prescription/api/urls.py
+++ prescription/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import PrescriptionGET, PrescriptionSAVE, Prescriptiondetailviews
+from .views import prescription_get, prescription_save
 
 urlpatterns = [
-    path('prescription/save', PrescriptionSAVE.as_view()),
-    path('prescription/get', PrescriptionGET.as_view()),
-    path('prescription/<int:id>', Prescriptiondetailviews.as_view()),
+    # path('prescription/save', PrescriptionSAVE.as_view()),
+    # path('prescription/get', PrescriptionGET.as_view()),
+    path('<int:id>', Prescriptiondetailviews.as_view()),
+    # path('prescription', prescription, name='home'),
+    path('get', prescription_get, name='prescription_get'),
+    path('save', prescription_save, name='prescription_save')
+
 ]
--- prescription/api/views.py
+++ prescription/api/views.py
@@ -3,11 +3,12 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from prescription.models import Prescription
 from .serializers import PrescriptionSerializer, PrescriptionViewSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+# Create your views here.
 class PrescriptionGET(ListCreateAPIView):
     serializer_class = PrescriptionViewSerializer
-    
     def get_queryset(self):
         return Prescription.objects.all()
     
@@ -15,11 +16,25 @@ class PrescriptionSAVE(ListCreateAPIView):
     serializer_class = PrescriptionSerializer
     def perform_create(self, serializer):
         return serializer.save()
-    
+
+
+@api_view(['GET'])
+def prescription_get(request):
+    data = Prescription.objects.all()
+    serializer = PrescriptionViewSerializer(data, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def prescription_save(request):
+    item = PrescriptionSerializer(data=request.data)
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 class Prescriptiondetailviews(RetrieveUpdateDestroyAPIView):
     serializer_class = PrescriptionViewSerializer
     lookup_field = 'id'
-    
     def get_queryset(self):
         return Prescription.objects.all()
