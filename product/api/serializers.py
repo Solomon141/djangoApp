ffcbdbf394c01f395be51a3e3ea5c9b1532f2146
solomon141 <weymit2001@gmail.com>
2023-11-08 08:35:40 +0300

    hospital id added to prescription

--- product/api/serializers.py
+++ product/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from product.models import Badreg
 
 class BadregSerializer(serializers.ModelSerializer):
+    value = serializers.IntegerField(source='id')
+    label = serializers.CharField(source='pname')
     class Meta:
         model = Badreg
-        fields = ['id', 'pname']
+        fields = ['id', 'pname', 'value', 'label']
